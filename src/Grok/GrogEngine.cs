ced7f673a2b4c9f2bbf0fbe19bd366256832b8f5
Pablo Vivera <pvivera@gmail.com>
2019-06-17 17:21:59 -0400

    Convertion after extraction

--- src/Grok/GrogEngine.cs
+++ src/Grok/GrogEngine.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 
@@ -12,21 +13,46 @@ namespace Grok
             {"NUMBER", @"\d+"},
         };
 
-        private static readonly Regex MatchValueRegex = new Regex(@"%{(\w+)(:(\w+)){0,1}}");
+        private static readonly Regex MatchValueRegex = new Regex(@"%{(\w+)((:(\w+)){0,1}|(:(\w+):(int|integer|long|double|boolean)){0,1})}");
 
-        private static readonly MatchEvaluator MatchValueEvaluator = match => match.Groups[2].Success
-            ? $"(?<{match.Groups[3].Value}>{Types[match.Groups[1].Value]})"
-            : $"({Types[match.Groups[1].Value]})";
+        private readonly IDictionary<string, string> _parameterConvert = new Dictionary<string, string>();
 
-        public static Dictionary<string, string> ExtractData(string pattern, string text)
+        private string ReplaceMatch(Match match)
         {
-            var replacedPattern = MatchValueRegex.Replace(pattern, MatchValueEvaluator);
+            var replacementPattern = string.Empty;
+
+            switch (match.Groups[0].Value.Split(':').Length - 1)
+            {
+                case 0:
+                    replacementPattern = $"({Types[match.Groups[1].Value]})";
+                    break;
+
+                case 1:
+                    replacementPattern = $"(?<{match.Groups[4].Value}>{Types[match.Groups[1].Value]})";
+                    break;
+
+                case 2:
+                    replacementPattern = $"(?<{match.Groups[6].Value}>{Types[match.Groups[1].Value]})";
+                    _parameterConvert.Add(match.Groups[6].Value, match.Groups[7].Value);
+                    break;
+            }
+
+            return replacementPattern;
+        }
+
+        public Dictionary<string, object> ExtractData(string pattern, string text)
+        {
+            var matchValueEvaluator = new MatchEvaluator(ReplaceMatch);
+
+            _parameterConvert.Clear();
+
+            var replacedPattern = MatchValueRegex.Replace(pattern, matchValueEvaluator);
 
             var regex = new Regex(replacedPattern);
 
             var matchCollection = regex.Match(text);
 
-            var result = new Dictionary<string, string>();
+            var result = new Dictionary<string, object>();
 
             if(!matchCollection.Success) return result;
 
@@ -34,10 +60,36 @@ namespace Grok
 
             foreach (var groupName in regex.GetGroupNames())
             {
-                result.Add(groupName, groups[groupName].Value);
+                result.Add(groupName,
+                    !_parameterConvert.ContainsKey(groupName)
+                        ? groups[groupName].Value
+                        : ConvertExtractedData(groupName, groups[groupName].Value));
             }
 
             return result;
+        }
+
+        private object ConvertExtractedData(string groupName, string value)
+        {
+            var convert = _parameterConvert[groupName];
+
+            switch (convert)
+            {
+                case "int":
+                case "integer":
+                    return Convert.ToInt32(value);
+
+                case "long":
+                    return Convert.ToInt64(value);
+
+                case "double":
+                    return Convert.ToDouble(value);
+
+                case "boolean":
+                    return Convert.ToBoolean(value);
+            }
+
+            return null;
         }
     }
 }
--- test/Grok.Tests/GrogEngineTests.cs
+++ test/Grok.Tests/GrogEngineTests.cs
@@ -5,10 +5,17 @@ namespace Grok.Tests
 {
     public class GrogEngineTests
     {
+        private readonly GrogEngine _sut;
+
+        public GrogEngineTests()
+        {
+            _sut = new GrogEngine();
+        }
+
         [Fact]
         public void ExtractData_ShouldExtractTheCorrectData()
         {
-            var data = GrogEngine.ExtractData("%{WORD:method} %{NUMBER:bytes} %{NUMBER:duration}", "hello 123 456");
+            var data = _sut.ExtractData("%{WORD:method} %{NUMBER:bytes} %{NUMBER:duration}", "hello 123 456");
 
             data.Should().ContainKey("method");
             data["method"].Should().Be("hello");
@@ -21,13 +28,26 @@ namespace Grok.Tests
         [Fact]
         public void ExtractData_ShouldIgnoreCapture_WhenThereIsNoParameterName()
         {
-            var data = GrogEngine.ExtractData("%{WORD} %{NUMBER:bytes} %{NUMBER:duration}", "hello 123 456");
+            var data = _sut.ExtractData("%{WORD} %{NUMBER:bytes} %{NUMBER:duration}", "hello 123 456");
 
             data.Should().NotContainKey("method");
             data.Should().ContainKey("bytes");
             data["bytes"].Should().Be("123");
             data.Should().ContainKey("duration");
             data["duration"].Should().Be("456");
+        }
+
+        [Fact]
+        public void ExtractData_ShouldExtractTheCorrectDataAndConvert()
+        {
+            var data = _sut.ExtractData("%{WORD:method} %{NUMBER:bytes:int} %{NUMBER:duration:integer}", "hello 123 456");
+
+            data.Should().ContainKey("method");
+            data["method"].Should().Be("hello");
+            data.Should().ContainKey("bytes");
+            data["bytes"].Should().Be(123);
+            data.Should().ContainKey("duration");
+            data["duration"].Should().Be(456);
         }
     }
 }
